48e6bd26d4b2da638035856ba8f98ccdc2de775c
ipnn4 <nicolasip@student.apc.edu.ph>
2024-04-03 22:15:32 +0800

    initial start

--- routes/web.php
+++ routes/web.php
@@ -23,6 +23,10 @@ Route::get('/dashboard', function () {
 })->middleware(['auth', 'verified'])->name('dashboard');
 
 Route::middleware('auth')->group(function () {
+    Route::get('/sports_match', function () {
+        return view('cruds.sports_match');
+    });
+    
     Route::get('/profile', [ProfileController::class, 'edit'])->name('profile.edit');
     Route::patch('/profile', [ProfileController::class, 'update'])->name('profile.update');
     Route::delete('/profile', [ProfileController::class, 'destroy'])->name('profile.destroy');
